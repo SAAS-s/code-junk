e11a64363e213be7f301a4d729f3f3c055ba5635
SAAS-s <surmai10131@gmail.com>
2024-06-07 12:12:29 +0530

    initial commit

---
+++ basic_datatypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int integer;
+    long long h;
+    char character;
+    float floatnumber;
+    double doublenum;
+    int n;
+    cin>>integer;
+    cin>> h;
+    cin>>character;
+    cin>>floatnumber;
+    cin>>doublenum;
+    
+    cout  << integer<< endl;
+    cout  << h << endl;
+    cout  << character<< endl;
+    cout  << floatnumber<< endl;
+    cout  << fixed <<setprecision(9)<<doublenum<<endl;
+
+    return 0;
+}
