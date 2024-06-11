97e77cc6e534a19805153139883b43ff4e4a0b33
SAAS-s <surmai10131@gmail.com>
2024-06-11 22:50:18 +0530

    file changed

--- merge_arrays.cpp
+++ merge_arrays.cpp
@@ -30,3 +30,24 @@ public:
     }
 };
 }
+/**
+ * Second approach
+ * class Solution {
+ * public:
+ *  string mergeAlternatively(string word1, string word2) {
+ *      int i = 0, j =0, n1 = word1.length(), n2 = word2.length();
+ *      string s = "";
+ *      while(i<n1 || j<n2){
+ *          if(i<n1){
+ *              s+= word1[i];
+ *              i++;
+ *          }
+ *          if(j<n2){
+ *              s+= word2[j];
+ *              j++;
+ *          }
+ *      }
+ *      return s;
+ *    }
+ * }
+ */
