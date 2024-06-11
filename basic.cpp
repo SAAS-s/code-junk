5747c6ccaf40d37d0073c12a4a8651589657ba9d
SAAS-s <surmai10131@gmail.com>
2024-06-11 22:12:21 +0530

    changes in commit history

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d;
+    int a = 10;
+    int b = 20;
+    int c = a+b;
+    cout<<"Hello world" <<endl;
+    cout<<"The addiction of a and b is :"<< c << endl;
+    return 0;
+}
