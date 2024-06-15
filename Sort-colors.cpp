47a162c6ab0fd171476c45ced23ec025a0a48e97
SAAS-s <surmai10131@gmail.com>
2024-06-15 22:08:25 +0530

    Reapply "codeforces-youknowwho"
    This reverts commit 4cf68e3a6e524b78ed73a2d4dbbdc93958dd7599.
    
    removing unnecessary additions

--- Sort-colors.cpp
+++ Sort-colors.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    
+    class Solution {
+        public:
+            void sortColors(vector<int>& nums) {
+                int zero = 0;
+                int end = nums.size() -1;
+
+                for(int i=0;i<=end;i++)
+                {
+                    while(nums[i]==2 && i<end) swap(nums[i], nums[end--]);
+                    while(nums[i]==0 && i>zero) swap(nums[i], nums[zero++]);
+                }
+            }
+    };
 }
