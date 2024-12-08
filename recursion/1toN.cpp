94e92603ec64d0982baa1b9fe4f1e23aa034e0b9
SAAS-s <surmai10131@gmail.com>
2024-12-08 02:18:52 -0500

    minor changes

--- recursion/1toN.cpp
+++ recursion/1toN.cpp
@@ -1,4 +1,4 @@
-/*pseudo-code
+/*pseudo-code for 1 to N
 f(i, N)
 {
 	if(i>N) return;
@@ -10,4 +10,22 @@ main(){
 	input(n)
 	f(1, N)
 }
+*/
+
+/* pseudo code for N to 1
+f(i, N)
+{
+	if(i<1) return;
+	print(i)
+	f(i-1, N)
+}
+main(){
+	input(n)
+	f(N,N)
+}
+*/
+//-------------------
+//Backtracking
+/*
+
 */
